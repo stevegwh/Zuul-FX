b93be987215466f812f762e54af3b4cc972b778f
Forest <sgwheeler@protonmail.com>
2019-11-02 17:30:21 +0000

    first commit

---
+++ Zuul-FX/src/zuul/View.java
@@ -0,0 +1,27 @@
+package zuul;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class View extends Application {
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("zuul.fxml"));
+		VBox vbox = loader.<VBox>load();
+
+		Scene scene = new Scene(vbox);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+}
