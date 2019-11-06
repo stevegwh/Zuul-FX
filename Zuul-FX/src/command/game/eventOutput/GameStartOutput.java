e5b9029642e19bddb7ad1cdbdb7c2cc591613d3d
Steven Wheeler <stevengwheeler@outlook.com>
2019-11-06 22:19:13 +0000

    Added cool title

--- Zuul-FX/src/command/game/eventOutput/GameStartOutput.java
+++ Zuul-FX/src/command/game/eventOutput/GameStartOutput.java
@@ -1,5 +1,10 @@
 package command.game.eventOutput;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import IO.IOHandler;
 import command.ICommandOutput;
 
@@ -13,11 +18,38 @@ public class GameStartOutput implements ICommandOutput {
 
 	@Override
 	public void init(String[] inputArray) {
-		IOHandler.output.println("");
-		IOHandler.output.println("Welcome to the World of Zuul!");
-		IOHandler.output.println("World of Zuul is a new, incredibly boring adventure game.");
-		IOHandler.output.println("Type 'help' if you need help.");
-		IOHandler.output.println("");
+        // The name of the file to open.
+		String fileName = "zuul_title.txt";
+        // This will reference one line at a time
+        String line = null;
+
+        try {
+            // FileReader reads text files in the default encoding.
+            InputStreamReader fileReader = new InputStreamReader(this.getClass().getResourceAsStream(fileName));
+
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader = 
+                new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null) {
+                IOHandler.output.println(line);
+            }   
+
+            // Always close files.
+            bufferedReader.close();         
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                "Unable to open file '" + 
+                fileName + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error reading file '" 
+                + fileName + "'");                  
+            // Or we could just do this: 
+            // ex.printStackTrace();
+        }
 	}
 
 }
